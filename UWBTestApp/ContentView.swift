214a235edf9751ae2ce4c8bcb0dc26e3766ee988
gus <andres.gonzalezpineda@gmail.com>
2025-05-29 14:43:01 -0600

    feat(UWBTestApp): device will now only connect to sensors if they are defined on the JSON map

--- UWBTestApp/ContentView.swift
+++ UWBTestApp/ContentView.swift
@@ -36,10 +36,11 @@ class EstimoteUWBManagerExample: NSObject, ObservableObject {
     private var uwbManager: EstimoteUWBManager?
     private var distances = Dictionary<String, Distances>(minimumCapacity: 3)
     private var trilateration: Trilateration
+    private var place_map: [String: Coordinate]
     
     override init() {
-        let json_map = MapReader.readCoordinates(from: "casa.json")
-        self.trilateration = Trilateration(map: json_map)
+        place_map = MapReader.readCoordinates(from: "casa.json")
+        self.trilateration = Trilateration(map: place_map)
     
         super.init()
         self.setupUWB()
@@ -83,13 +84,26 @@ extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
     func didUpdatePosition(for device: EstimoteUWBDevice) {
         self.distances[device.publicIdentifier] = Distances(distance: device.distance, timestamp: Date().timeIntervalSince1970)
         let top3 = self.trilateration.pickCoordinates(distances: self.distances)
-        let coord: Coordinate = self.trilateration.tirlaterate(top3Distances: top3)
+        
+        let coord: Coordinate
+        if top3.count == 3{
+            coord = self.trilateration.tirlaterate(top3Distances: top3)
+        }else{
+            coord = Coordinate(x: 0, y: 0, z: 0)
+        }
         print("Estimated Position: \(coord)")
     }
     
     // OPTIONAL
     func didDiscover(device: UWBIdentifiable, with rssi: NSNumber, from manager: EstimoteUWBManager) {
         print("Discovered: \(device)")
+        print(self.distances[device.publicIdentifier] == nil)
+        
+        // We can only connect to the sensor if it is on the place_map
+        guard place_map[device.publicIdentifier] != nil else {
+                return 
+        }
+        
         if self.distances[device.publicIdentifier] == nil && self.distances.count < self.connNumber {
             self.distances[device.publicIdentifier] = Distances(distance: 0, timestamp: Date().timeIntervalSince1970)
             self.uwbManager?.connect(to: device.publicIdentifier)
@@ -98,7 +112,7 @@ extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
     
     // OPTIONAL
     func didConnect(to device: UWBIdentifiable) {
-        
+        print("Connected to: \(device)")
     }
     
     // OPTIONAL
