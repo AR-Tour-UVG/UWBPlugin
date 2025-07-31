b98dc1d8dabb0ae8c7225450872cc9859ba78ab1
gus <andres.gonzalezpineda@gmail.com>
2025-07-30 20:05:30 -0600

    feat(plugin): ObjectiveC function exposed for Unity plugin

---
+++ UWBplugin/Main.swift
@@ -0,0 +1,40 @@
+//
+//  Main.swift
+//  UWBplugin
+//
+//  Created by Gustavo Gonzalez on 7/23/25.
+//
+
+import Foundation
+
+
+let viewModel = ViewModel()
+let positionCoordinator = viewModel.getPositionCoordinator()
+
+@_cdecl("getCoords")
+func getCoords() -> UnsafeMutablePointer<CChar> {
+    var coords: [String: Any]  // Use 'Any' so we can put Double or nil
+    
+    if let position = positionCoordinator.getFilteredPosition() {
+        coords = [
+            "x": position.x,
+            "y": position.y
+        ]
+    } else {
+        coords = [
+            "x": NSNull(),  // JSON-compatible representation of null
+            "y": NSNull()
+        ]
+    }
+    
+    if let jsonData = try? JSONSerialization.data(withJSONObject: coords, options: []),
+       let jsonString = String(data: jsonData, encoding: .utf8) {
+        
+        // Return a C string (null-terminated UTF8)
+        let cString = strdup(jsonString)
+        return UnsafeMutablePointer(cString!)
+    } else {
+        // Return an empty JSON object on error
+        return strdup("{}")
+    }
+}
---
+++ UWBplugin/Models/ViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ViewModel.swift
+//  UWBplugin
+//
+//  Created by Gustavo Gonzalez on 7/21/25.
+//
+import Foundation
+
+
+class ViewModel{
+    // these should be environment variables
+    var mapURL = "casa.json"
+    var refreshRate = Float(0.1)
+    var connectionLimit = 6
+    var connectionTimout = 10.0
+    let posCoordinator: PositionCoordinator
+    
+    var anchorMap: [String: Vector3D]
+    
+    init(){
+        // We first read the Campus Map
+        self.anchorMap = MapReader.readCoordinates(from: mapURL)
+        
+        //We initialize a single operation queue for the Accelerometer and UWB modules
+        let queue = OperationQueue()
+        queue.name = "UWB and Accelerometer queue"
+        queue.maxConcurrentOperationCount = 1
+        
+        posCoordinator = PositionCoordinator(anchorsPositions: anchorMap, connectionLimit: connectionLimit, connectionTimeout: connectionTimout)
+        
+        let accelerometerManager = AccelerometerManager(refreshRate: refreshRate, delegate: posCoordinator, accelerometerQueue: queue)
+        let uwbManager = UWBManager(delegate: posCoordinator, UWBQueue: queue)
+        uwbManager.startScanning()
+        accelerometerManager.startListening()
+        
+        // IMPORTANT: add the delegates to the Position Coordinator
+        posCoordinator.accelerometerManager = accelerometerManager
+        posCoordinator.uwbManager = uwbManager
+    }
+    
+    func getPositionCoordinator() -> PositionCoordinator{
+        return self.posCoordinator
+    }
+}
